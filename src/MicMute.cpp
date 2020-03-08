5736738cd4e72c47da24f4a1603b72637ab68bce
Aaron Erlandson <aaroner@microsoft.com>
2020-03-08 09:20:19 -0700

    First working code

--- src/MicMute.cpp
+++ src/MicMute.cpp
@@ -2,10 +2,153 @@
 //
 
 #include <iostream>
+#include <stdio.h>
+#include <windows.h>
+#include <mmdeviceapi.h>
+#include <endpointvolume.h>
+#include <Functiondiscoverykeys_devpkey.h>
+#include "MicMute.h"
+
+#define EXIT_ON_ERROR(hres)  \
+              if (FAILED(hres)) { goto Exit; }
+#define SAFE_RELEASE(punk)  \
+              if ((punk) != NULL)  \
+                { (punk)->Release(); (punk) = NULL; }
+
+const CLSID CLSID_MMDeviceEnumerator = __uuidof(MMDeviceEnumerator);
+const IID IID_IMMDeviceEnumerator = __uuidof(IMMDeviceEnumerator);
+
+void PrintEndpointNames()
+{
+    HRESULT hr = S_OK;
+    IMMDeviceEnumerator* pEnumerator = NULL;
+    IMMDeviceCollection* pCollection = NULL;
+    IMMDevice* pEndpoint = NULL;
+    IPropertyStore* pProps = NULL;
+    LPWSTR pwszID = NULL;
+
+    hr = CoCreateInstance(
+        CLSID_MMDeviceEnumerator, NULL,
+        CLSCTX_ALL, IID_IMMDeviceEnumerator,
+        (void**)&pEnumerator);
+    EXIT_ON_ERROR(hr)
+
+        hr = pEnumerator->EnumAudioEndpoints(
+            eCapture, DEVICE_STATE_ACTIVE,
+            &pCollection);
+    EXIT_ON_ERROR(hr)
+
+        UINT  count;
+    hr = pCollection->GetCount(&count);
+    EXIT_ON_ERROR(hr)
+
+        if (count == 0)
+        {
+            printf("No endpoints found.\n");
+        }
+
+    // Each loop prints the name of an endpoint device.
+    for (ULONG i = 0; i < count; i++)
+    {
+        // Get pointer to endpoint number i.
+        hr = pCollection->Item(i, &pEndpoint);
+        EXIT_ON_ERROR(hr)
+
+            // Get the endpoint ID string.
+            hr = pEndpoint->GetId(&pwszID);
+        EXIT_ON_ERROR(hr)
+
+            hr = pEndpoint->OpenPropertyStore(
+                STGM_READ, &pProps);
+        EXIT_ON_ERROR(hr)
+
+            PROPVARIANT varName;
+        // Initialize container for property value.
+        PropVariantInit(&varName);
+
+        // Get the endpoint's friendly-name property.
+        hr = pProps->GetValue(
+            PKEY_Device_FriendlyName, &varName);
+        EXIT_ON_ERROR(hr)
+
+            // Print endpoint friendly name and endpoint ID.
+            printf("Endpoint %d: \"%S\" (%S)\n",
+                i, varName.pwszVal, pwszID);
+
+        CoTaskMemFree(pwszID);
+        pwszID = NULL;
+        PropVariantClear(&varName);
+        SAFE_RELEASE(pProps)
+            SAFE_RELEASE(pEndpoint)
+    }
+    SAFE_RELEASE(pEnumerator)
+        SAFE_RELEASE(pCollection)
+        return;
+
+Exit:
+    printf("Error!\n");
+    CoTaskMemFree(pwszID);
+    SAFE_RELEASE(pEnumerator)
+        SAFE_RELEASE(pCollection)
+        SAFE_RELEASE(pEndpoint)
+        SAFE_RELEASE(pProps)
+}
+
+int GetCaptureId()
+{
+    HRESULT hr;
+
+    IMMDeviceEnumerator* deviceEnumerator = NULL;
+    hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_INPROC_SERVER, __uuidof(IMMDeviceEnumerator), (LPVOID*)&deviceEnumerator);
+    if (FAILED(hr)) {
+        std::cout << "CoCreateInstance failed, hr = " << std::hex << hr;
+        return 1;
+    }
+
+    IMMDevice* defaultDevice = NULL;
+
+    hr = deviceEnumerator->GetDefaultAudioEndpoint(eCapture, eConsole, &defaultDevice);
+    deviceEnumerator->Release();
+    deviceEnumerator = NULL;
+
+    if (FAILED(hr)) {
+        std::cout << "GetDefaultAudioEndpoint failed, hr = " << std::hex << hr;
+        return 2;
+    }
+
+    WCHAR* deviceId;
+    hr = defaultDevice->GetId(&deviceId);
+    if (FAILED(hr)) {
+        std::cout << "GetId failed, hr = " << std::hex << hr;
+        return 3;
+    }
+
+    defaultDevice->Release();
+    defaultDevice = NULL;
+
+    std::cout << "Default device id: " << deviceId;
+
+    CoTaskMemFree(deviceId);
+
+    return 0;
+}
 
 int main()
 {
     std::cout << "Hello World!\n";
+
+    HRESULT hr;
+    hr = CoInitialize(NULL);
+    if (FAILED(hr)) {
+        std::cout << "CoInitialize failed, hr = " << std::hex << hr;
+        return 1;
+    }
+    // PrintEndpointNames();
+    GetCaptureId();
+
+    CoUninitialize();
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
---
+++ src/MicMute.h
@@ -0,0 +1,3 @@
+#pragma once
+
+int GetCaptureId(HRESULT& hr, bool& retflag);
